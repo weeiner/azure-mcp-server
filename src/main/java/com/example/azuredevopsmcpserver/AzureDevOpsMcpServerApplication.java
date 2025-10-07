defc02a00bba17cffd51c1835d4093623ccfbf6f
weeiner <71634275+weeiner@users.noreply.github.com>
2025-10-07 19:02:53 +0800

    feat: added library to get env var

--- src/main/java/com/example/azuredevopsmcpserver/AzureDevOpsMcpServerApplication.java
+++ src/main/java/com/example/azuredevopsmcpserver/AzureDevOpsMcpServerApplication.java
@@ -3,8 +3,14 @@ package com.example.azuredevopsmcpserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class AzureDevOpsMcpServerApplication {
+
+    Dotenv dotenv = Dotenv.load();
+    String org = dotenv.get("AZURE_DEVOPS_ORGANIZATION");
+    String pat = dotenv.get("AZURE_DEVOPS_PAT");
 
     public static void main(String[] args) {
         SpringApplication.run(AzureDevOpsMcpServerApplication.class, args);
